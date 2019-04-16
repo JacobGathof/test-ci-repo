462476041de2d20515f22a4f4cd96258704e2a0f
Gandalf <Gandalf@DESKTOP-V4DFHOO>
2019-04-16 16:24:26 -0400

    Install docfx

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,6 @@ echo "Testing Build Process"
 
 sudo apt-get update
 sudo apt-get install unzip
-sudo apt-get install mono-devel
 
 wget -O docfxzip.zip https://github.com/dotnet/docfx/releases/download/v2.41/docfx.zip
 unzip docfxzip.zip -d dfxd
