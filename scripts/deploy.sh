05ddecebcda0062d24bc27b8c50c74fd04d06920
Gandalf <Gandalf@DESKTOP-V4DFHOO>
2019-04-16 15:26:09 -0400

    Install docfx

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,8 @@
 set -ev
 echo "Testing Build Process"
 ls
-brew install docfx
-docfx docfx/docfx.json
+sudo apt-get install unzip
+wget -O docfxzip.zip https://github.com/dotnet/docfx/releases/download/v2.41/docfx.zip
+mkdir dfxd
+unzip docfxzip.zip -d dfxd
+dfxd/docfx.exe docfx/docfx.json
