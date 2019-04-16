cc47d183a987a086fca6cb0cc8b18f6a56879794
Gandalf <Gandalf@DESKTOP-V4DFHOO>
2019-04-16 16:02:58 -0400

    Install docfx

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -ev
 echo "Testing Build Process"
-choco install docfx -y
-docfx docfx/docfx.json
+
+sudo apt-get install unzip
+sudo apt install mono-complete
+wget -O docfxzip.zip https://github.com/dotnet/docfx/releases/download/v2.41/docfx.zip
+unzip docfxzip.zip -d ~/dfxd
+
+mono dfxd/docfx docfx/docfx.json
