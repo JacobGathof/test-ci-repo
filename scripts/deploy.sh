22f6e394f6c614814ef0491e225964d797c854a4
Gandalf <Gandalf@DESKTOP-V4DFHOO>
2019-04-16 15:49:17 -0400

    Install docfx

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,5 +6,5 @@ sudo apt-get install unzip
 wget -O docfxzip.zip https://github.com/dotnet/docfx/releases/download/v2.41/docfx.zip
 unzip docfxzip.zip -d ~/dfxd
 ls
-chmod +x ~/dfxd/docfx.exe
-~/dfxd/docfx.exe docfx/docfx.json
+chmod +x ~/dfxd/docfx
+~/dfxd/docfx docfx/docfx.json
