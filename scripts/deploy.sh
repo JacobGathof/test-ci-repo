c0004c8960ed0c6cdbb5877c817088057ed0585e
Gandalf <Gandalf@DESKTOP-V4DFHOO>
2019-04-16 16:07:22 -0400

    Install docfx

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,6 @@ sudo apt-get install unzip
 sudo apt-get install mono-complete
 
 wget -O docfxzip.zip https://github.com/dotnet/docfx/releases/download/v2.41/docfx.zip
-unzip docfxzip.zip -d ~/dfxd
+unzip docfxzip.zip -d dfxd
 
 mono dfxd/docfx docfx/docfx.json
