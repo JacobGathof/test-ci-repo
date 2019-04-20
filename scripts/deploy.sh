77133e278323d8d1ffef21467ed6dd7819abd8c5
Gandalf <Gandalf@DESKTOP-V4DFHOO>
2019-04-20 15:23:29 -0400

    New Test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,17 +2,26 @@
 set -ev
 echo "Testing Build Process"
 
-sudo apt-get install unzip
+#sudo apt-get install unzip
 
-wget -O docfxzip.zip https://github.com/dotnet/docfx/releases/download/v2.41/docfx.zip
-unzip docfxzip.zip -d dfxd
+#wget -O docfxzip.zip https://github.com/dotnet/docfx/releases/download/v2.41/docfx.zip
+#unzip docfxzip.zip -d dfxd
 
-mono dfxd/docfx.exe docfx.json
+#mono dfxd/docfx.exe docfx.json
 
-shopt -s extglob 
-rm -rf !(docs)
-cd docs
-mv * ../
-cd ..
-rm -rf docs
-ls
+#shopt -s extglob 
+#rm -rf !(temp)
+#cd temp
+#mv * ../
+#cd ..
+#rm -rf temp
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+
+git checkout -b gh-pages
+git add .
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+
+git remote add origin https://${GITHUB_API_KEY}@github.com/JacobGathof/test-ci-repo.git
+git push -u origin gh-pages 
